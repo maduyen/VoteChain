facc39000843457a4e277a4064be3b8616b7d088
Naitik Jain <naitikjain@Naitiks-MacBook-Air.local>
2023-12-03 02:08:18 -0800

    Made changes in Login and Signup Page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,98 @@
+// import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+
+// import InputControl  from "../InputControl/InputContol";
+// import { auth } from "../../firebase";
+
+// import styles from "./Login.module.css";
+
+// function Login() {
+//   const navigate = useNavigate();
+//   const [values, setValues] = useState({
+//     email: "",
+//     pass: "",
+//   });
+//   const [errorMsg, setErrorMsg] = useState("");
+//   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+//   const handleSubmission = () => {
+//     if (!values.email || !values.pass) {
+//       setErrorMsg("Fill all fields");
+//       return;
+//     }
+//     setErrorMsg("");
+
+//     setSubmitButtonDisabled(true);
+
+//     // Will redirect to results page
+
+//     if (values.email === "n@gmail.com" && values.pass === "123456") {
+//       // Redirect to Candidates page
+//       navigate("/candidates");
+//       setSubmitButtonDisabled(false);
+//       return;
+//     }
+
+//     signInWithEmailAndPassword(auth, values.email, values.pass)
+//       .then(async (res) => {
+//         setSubmitButtonDisabled(false);
+        
+//         navigate("/elections");
+//       })
+//       .catch((err) => {
+//         setSubmitButtonDisabled(false);
+//         setErrorMsg(err.message);
+//       });
+//   };
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.innerBox}>
+//         <h1 className={styles.heading}>Login</h1>
+
+//         <InputControl
+//           label="Email"
+//           onChange={(event) =>
+//             setValues((prev) => ({ ...prev, email: event.target.value }))
+//           }
+//           placeholder="Enter email address"
+//         />
+//         <InputControl
+//           label="Password"
+//           type="password"
+//           onChange={(event) =>
+//             setValues((prev) => ({ ...prev, pass: event.target.value }))
+//           }
+//           placeholder="Enter Password"
+          
+//         />
+
+//         <div className={styles.footer}>
+//           <b className={styles.error}>{errorMsg}</b>
+//           <button disabled={submitButtonDisabled} onClick={handleSubmission}>
+//             Login
+//           </button>
+//           <p>
+//             Already have an account?{" "}
+//             <span>
+//               <Link to="/signup">Signup</Link>
+//             </span>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-
-import InputControl  from "../InputControl/InputContol";
 import { auth } from "../../firebase";
-
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
 import styles from "./Login.module.css";
-
+import loginImage from "../images/Untitled_design-removebg-preview.png"
 function Login() {
   const navigate = useNavigate();
   const [values, setValues] = useState({
@@ -37,7 +123,6 @@ function Login() {
     signInWithEmailAndPassword(auth, values.email, values.pass)
       .then(async (res) => {
         setSubmitButtonDisabled(false);
-        
         navigate("/elections");
       })
       .catch((err) => {
@@ -45,44 +130,70 @@ function Login() {
         setErrorMsg(err.message);
       });
   };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.innerBox}>
-        <h1 className={styles.heading}>Login</h1>
+    <Container className={styles.container}>
+      {/* <Row className="justify-content-center"> */}
+<Row className="justify-content-center">
+      <Col md="6" className={styles.imageCol}>
+          {/* Left half of the page for the image */}
+          <div className={styles.imageContainer}>
+            <img src={loginImage} alt="Login" className={styles.loginImage} />
+          </div>
+        </Col>
+        <Col md="6">
+          <div className={styles.innerBox}>
+            <h1 className={styles.heading}>Login</h1>
 
-        <InputControl
-          label="Email"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, email: event.target.value }))
-          }
-          placeholder="Enter email address"
-        />
-        <InputControl
-          label="Password"
-          type="password"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, pass: event.target.value }))
-          }
-          placeholder="Enter Password"
-          
-        />
+            <Form>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="Enter email address"
+                  value={values.email}
+                  onChange={(event) =>
+                    setValues((prev) => ({ ...prev, email: event.target.value }))
+                  }
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                  value={values.pass}
+                  onChange={(event) =>
+                    setValues((prev) => ({ ...prev, pass: event.target.value }))
+                  }
+                />
+              </FormGroup>
 
-        <div className={styles.footer}>
-          <b className={styles.error}>{errorMsg}</b>
-          <button disabled={submitButtonDisabled} onClick={handleSubmission}>
-            Login
-          </button>
-          <p>
-            Already have an account?{" "}
-            <span>
-              <Link to="/signup">Signup</Link>
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
+              {errorMsg && <Alert color="danger">{errorMsg}</Alert>}
+
+              <div className={styles.footer}>
+                <Button
+                  color="primary"
+                  disabled={submitButtonDisabled}
+                  onClick={handleSubmission}
+                >
+                  {submitButtonDisabled ? "Logging in..." : "Login"}
+                </Button>
+                <p>
+                  Already have an account?{" "}
+                  <span>
+                    <Link to="/signup">Signup</Link>
+                  </span>
+                </p>
+              </div>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 export default Login;
-
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -86,12 +86,116 @@
 
 // export default Signup;
 
+// import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+// import InputControl from "../InputControl/InputContol";
+// import { auth } from "../../firebase";
+// import styles from "./Signup.module.css";
+
+// function Signup() {
+//   const navigate = useNavigate();
+//   const [values, setValues] = useState({
+//     name: "",
+//     email: "",
+//     pass: "",
+//   });
+//   const [errorMsg, setErrorMsg] = useState("");
+//   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+//   const handleSubmission = () => {
+//     if (!values.name || !values.email || !values.pass) {
+//       setErrorMsg("Fill all fields");
+//       return;
+//     }
+//     setErrorMsg("");
+
+//     setSubmitButtonDisabled(true);
+//     createUserWithEmailAndPassword(auth, values.email, values.pass)
+//       .then(async (res) => {
+//         setSubmitButtonDisabled(false);
+//         const user = res.user;
+//         await updateProfile(user, {
+//           displayName: values.name,
+//         });
+//         navigate("/");
+//       })
+//       .catch((err) => {
+//         setSubmitButtonDisabled(false);
+//         setErrorMsg(err.message);
+//       });
+//   };
+
+//   const handlePasswordChange = (event) => {
+//     const value = event.target.value;
+//     setValues((prev) => ({
+//       ...prev,
+//       pass: value,
+//     }));
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.innerBox}>
+//         <h1 className={styles.heading}>Signup</h1>
+
+//         <InputControl
+//           label="Name"
+//           placeholder="Enter your name"
+//           onChange={(event) =>
+//             setValues((prev) => ({ ...prev, name: event.target.value }))
+//           }
+//         />
+//         <InputControl
+//           label="Email"
+//           placeholder="Enter email address"
+//           onChange={(event) =>
+//             setValues((prev) => ({ ...prev, email: event.target.value }))
+//           }
+//         />
+//         <InputControl
+//           label="Password"
+//           placeholder="Enter password"
+//           type="password" // Set the input type to "password"
+//           value={values.pass}
+//           onChange={handlePasswordChange} // Handle password changes
+//         />
+
+//         <div className={styles.footer}>
+//           <b className={styles.error}>{errorMsg}</b>
+//           <button onClick={handleSubmission} disabled={submitButtonDisabled}>
+//             Signup
+//           </button>
+//           <p>
+//             Already have an account?{" "}
+//             <span>
+//               <Link to="/login">Login</Link>
+//             </span>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Signup;
+
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import InputControl from "../InputControl/InputContol";
 import { auth } from "../../firebase";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
 import styles from "./Signup.module.css";
+import signupImage from "../images/Untitled_design-removebg-preview.png"; // Replace with your image path
 
 function Signup() {
   const navigate = useNavigate();
@@ -126,56 +230,86 @@ function Signup() {
       });
   };
 
-  const handlePasswordChange = (event) => {
-    const value = event.target.value;
+  const handleInputChange = (event, field) => {
+    const { value } = event.target;
     setValues((prev) => ({
       ...prev,
-      pass: value,
+      [field]: value,
     }));
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.innerBox}>
-        <h1 className={styles.heading}>Signup</h1>
-
-        <InputControl
-          label="Name"
-          placeholder="Enter your name"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, name: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Email"
-          placeholder="Enter email address"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, email: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Password"
-          placeholder="Enter password"
-          type="password" // Set the input type to "password"
-          value={values.pass}
-          onChange={handlePasswordChange} // Handle password changes
-        />
-
-        <div className={styles.footer}>
-          <b className={styles.error}>{errorMsg}</b>
-          <button onClick={handleSubmission} disabled={submitButtonDisabled}>
-            Signup
-          </button>
-          <p>
-            Already have an account?{" "}
-            <span>
-              <Link to="/login">Login</Link>
-            </span>
-          </p>
-        </div>
-      </div>
+      <Container fluid>
+        <Row>
+          <Col md={{ size: 4, offset: 1 }}>
+            <div className={styles.innerBox}>
+              <h1 className={styles.heading}>Signup</h1>
+              <Form>
+                <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input
+                    type="text"
+                    id="name"
+                    placeholder="Enter your name"
+                    value={values.name}
+                    onChange={(e) => handleInputChange(e, "name")}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="email">Email</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="Enter email address"
+                    value={values.email}
+                    onChange={(e) => handleInputChange(e, "email")}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="Enter password"
+                    value={values.pass}
+                    onChange={(e) => handleInputChange(e, "pass")}
+                  />
+                </FormGroup>
+                <div className={styles.footer}>
+                  <b className={styles.error}>{errorMsg}</b>
+                  <Button
+                    color="primary"
+                    onClick={handleSubmission}
+                    disabled={submitButtonDisabled}
+                  >
+                    Signup
+                  </Button>
+                  <p>
+                    Already have an account?{" "}
+                    <span>
+                      <Link to="/login">Login</Link>
+                    </span>
+                  </p>
+                </div>
+              </Form>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className={styles.imageContainer}>
+              <img
+                src={signupImage}
+                alt="Signup"
+                className={styles.signupImage}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default Signup;
+
+// Update the CSS as per your styling requirements for container, innerBox, footer, etc.
