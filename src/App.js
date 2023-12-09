15d9485a9136ab81f2fae72458dacae2ca72b499
Kri-hika <krithika.n@somaiya.edu>
2023-12-08 18:50:52 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
