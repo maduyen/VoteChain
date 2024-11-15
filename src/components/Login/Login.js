5652106590628d592b68a1a55e057c94b776620a
Shengzhe Zhang <ShZhang0907@gmail.com>
2024-11-14 21:08:16 -0800

    delete comments

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,91 +1,3 @@
-// import React, { useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import { signInWithEmailAndPassword } from "firebase/auth";
-
-// import InputControl  from "../InputControl/InputContol";
-// import { auth } from "../../firebase";
-
-// import styles from "./Login.module.css";
-
-// function Login() {
-//   const navigate = useNavigate();
-//   const [values, setValues] = useState({
-//     email: "",
-//     pass: "",
-//   });
-//   const [errorMsg, setErrorMsg] = useState("");
-//   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
-
-//   const handleSubmission = () => {
-//     if (!values.email || !values.pass) {
-//       setErrorMsg("Fill all fields");
-//       return;
-//     }
-//     setErrorMsg("");
-
-//     setSubmitButtonDisabled(true);
-
-//     // Will redirect to results page
-
-//     if (values.email === "n@gmail.com" && values.pass === "123456") {
-//       // Redirect to Candidates page
-//       navigate("/candidates");
-//       setSubmitButtonDisabled(false);
-//       return;
-//     }
-
-//     signInWithEmailAndPassword(auth, values.email, values.pass)
-//       .then(async (res) => {
-//         setSubmitButtonDisabled(false);
-        
-//         navigate("/elections");
-//       })
-//       .catch((err) => {
-//         setSubmitButtonDisabled(false);
-//         setErrorMsg(err.message);
-//       });
-//   };
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.innerBox}>
-//         <h1 className={styles.heading}>Login</h1>
-
-//         <InputControl
-//           label="Email"
-//           onChange={(event) =>
-//             setValues((prev) => ({ ...prev, email: event.target.value }))
-//           }
-//           placeholder="Enter email address"
-//         />
-//         <InputControl
-//           label="Password"
-//           type="password"
-//           onChange={(event) =>
-//             setValues((prev) => ({ ...prev, pass: event.target.value }))
-//           }
-//           placeholder="Enter Password"
-          
-//         />
-
-//         <div className={styles.footer}>
-//           <b className={styles.error}>{errorMsg}</b>
-//           <button disabled={submitButtonDisabled} onClick={handleSubmission}>
-//             Login
-//           </button>
-//           <p>
-//             Already have an account?{" "}
-//             <span>
-//               <Link to="/signup">Signup</Link>
-//             </span>
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -133,9 +45,8 @@ function Login() {
 
   return (
     <Container className={styles.container}>
-      {/* <Row className="justify-content-center"> */}
-<Row className="justify-content-center">
-      <Col md="6" className={styles.imageCol} style={{paddingRight: "50px"}}>
+      <Row className="justify-content-center">
+        <Col md="6" className={styles.imageCol} style={{paddingRight: "50px"}}>
           {/* Left half of the page for the image */}
           <div className={styles.imageContainer}>
             <img src={loginImage} alt="Login" className={styles.loginImage} />
@@ -172,7 +83,6 @@ function Login() {
               </FormGroup>
               <div></div>
 
-              {/* {errorMsg && <Alert color="danger">{errorMsg}</Alert>} */}
               {errorMsg && <Alert color="danger">Invalid Credentials</Alert>}
               <div className={styles.footer}>
                 <Button
