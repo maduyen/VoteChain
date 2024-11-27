c613cac904b0dc28520017a33085f1db23c337fb
Madelyn Nguyen <madelynguyen@gmail.com>
2024-11-27 08:51:59 -0800

    Home wip: added Slider feature to "How Does VoteChain Work"

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
 import { Row, Col } from "reactstrap";
-import CardContent from "@material-ui/core/CardContent";
 import Navbar from "../Navbar1";
 import Footer from "../Footer2";
 import voting_home from "../../components/images/voting_home_2.svg";
 import election from "../../components/images/election.svg";
-import report from "../../components/images/report.svg";
 import discussion from "../../components/images/discussion.svg";  //added discussion panel image to "images" folder
-import secure from "../../components/images/secure.svg";
 import transparent from "../../components/images/Electronic Vote Visualisation in the Admin panel.svg";
 import immutable from "../../components/images/Immutable_votes.svg";
-import singleVoting from "../../components/images/Single_voting_instance.svg";
-import restrictedAccess from "../../components/images/Restricted_access.svg";
 import blockchain from "../../components/images/blockchain.svg"  //added blockchain image to "images" folder
 import customize from "../../components/images/customize.svg"  //added customizable voting mech image to "images" folder
 import immutable_vote from "../../components/images/immutable_vote.svg"  //added immutable vote image to "images folder"
+import signup from "../../components/images/signup.svg"
 import { Button } from "reactstrap";
+
+//adding react-slick for carousel/slider
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -44,18 +42,21 @@ const useStyles = makeStyles((theme) => ({
 function Home(props) {
   const classes = useStyles();
 
+  //settings for carousel/Slider
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <div className="wrapper" style={{ paddingLeft: "2rem", paddingRight: "2rem" }}>
-        <div className="page-header header-filter">
-          <Container>
-            <div className="content-center brand">
-            </div>
-          </Container>
-        </div>
         <section className="section section-lg section-safe">
           <Container>
             <Row className="row-grid justify-content-between">
@@ -89,73 +90,33 @@ function Home(props) {
           <Container>
             <div className="content-center brand">
               <hr className="line-success" />
-              <h1 className="display-2" style={{ color: "#221c1c" }}>How Does VoteChain Work?</h1>
+              <h1 className="display-2 text-center" style={{ color: "#221c1c" }}>How Does VoteChain Work?</h1>
             </div>
           </Container>
         </div>
 
+        <div className="w-3/4 m-auto">
+          <Slider {...settings}>
+            {data.map((d) => (
+              <div className="bg-white h-[420px] text-black rounded-xl">
+                <div className="h-60 bg-white flex justify-center items-center rounded-t-xl">
+                  <img 
+                  style={{ width: '200px', height: '200px' }}
+                  className="img-fluid floating"
+                  src={d.image} 
+                  alt="..."/>
+                </div>
+              
+                <div className="flex flex-col jusify-center items-center p-4">
+                  <p className="text-xl font-semibold">{d.section}</p>
+                  <p>{d.text}</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+
         <Container className="classes.usestyles">
-          <div style={{ padding: 30 }} >
-            <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
-                <Card className={classes.card}>
-                  <CardContent style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <img
-                      alt="..."
-                      // src={signup}
-                      style={{ width: '300px', height: '300px' }} />
-                    <h3 style={{ color: "#8fac86", marginBottom: "20px"}}>Registration</h3>
-                    <h6 style={{ color: "#555b69"}}>
-                      Create an account today to participate in the upcoming polls! Each voter is assigned a unique digital token that is stored on the blockchain, ensuring identity integrity and preventing unauthorized access.
-                    </h6>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Card className={classes.card}>
-                  <CardContent style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <img
-                      alt="..."
-                      src={election}
-                      style={{ width: '300px', height: '300px' }} />
-                    <h3 style={{ color: "#8fac86", marginBottom: "20px" }}>Get Ready to Vote!</h3>
-                    <h6 style={{ color: "#555b69"}}>
-                      Dive into the details and vote for what you believe in. Explore the topic pages to find essential information about the polls and be prepared to make your voice heard. Your participation is crucial for shaping our community!
-                    </h6>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Card className={classes.card}>
-                  <CardContent style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <img
-                      alt="..."
-                      src={discussion}  //image for discussion panel description
-                      style={{ width: '300px', height: '300px' }} />
-                    <h3 style={{ color: "#8fac86", marginBottom: "20px" }}>Participate in Discussions
-                    </h3>
-                    <h6 style={{ color: "#555b69"}}>
-                      Poll Creators can create configurable discussion panels with options for public or private access, integrating anonymity features. Users now have the opportunity to provide feedback and engage in conversations about voting topics! Please be mindful when participating 😊
-                    </h6>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Card className={classes.card}>
-                  <CardContent style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <img
-                      alt="..."
-                      src={blockchain}  //image for infrastructure description
-                      style={{ width: '300px', height: '300px' }} />
-                    <h3 style={{ color: "#8fac86", marginBottom: "20px" }}>Secured by Blockchain
-                    </h3>
-                    <h6 style={{ color: "#555b69"}}>
-                      VoteChain is powered by the ResilientDB and Graph QL blockchain infrastructure. By decentralizing our voting system, we guarantee that voting data is secure and immutable. Poll outcomes are fully determined by the people, creating a transparent voting environment.
-                    </h6>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
             <div className="content-center brand">
               <hr className="line-success" />
               <h1 className="display-2" style={{ color: "#221c1c", paddingBottom: "25px"}}>Features</h1>
@@ -198,7 +159,6 @@ function Home(props) {
                 </div>
               </div>
             </div>
-          </div>
         </Container>
 
       </div>
@@ -209,5 +169,30 @@ function Home(props) {
     </div>
   );
 }
+
+const data = [
+  {
+    image: signup,
+    section: 'Registration',
+    text: 'Create an account today to participate in the upcoming polls! Each voter is assigned a unique digital token that is stored on the blockchain, ensuring identity integrity and preventing unauthorized access.'
+  },
+  {
+    image: election,
+    section: 'Get Ready to Vote!',
+    text: 'Dive into the details and vote for what you believe in. Explore the topic pages to find essential information about the polls and be prepared to make your voice heard. Your participation is crucial for shaping our community!'
+  },
+  {
+    image: discussion,
+    section: 'Participate in Discussions',
+    text: 'Poll Creators can create configurable discussion panels with options for public or private access, integrating anonymity features. Users now have the opportunity to provide feedback and engage in conversations about voting topics! Please be mindful when participating 😊'
+  },
+  {
+    image: blockchain,
+    section: 'Secured by Blockchain',
+    text: 'VoteChain is powered by the ResilientDB and Graph QL blockchain infrastructure. By decentralizing our voting system, we guarantee that voting data is secure and immutable. Poll outcomes are fully determined by the people, creating a transparent voting environment.'
+  }
+]
+
+
 export default Home;
 
