1097d6d472bfa28fb8b62b69812c1fde5e5f519a
Madelyn Nguyen <madelynguyen@gmail.com>
2024-12-06 15:13:27 -0800

    DiscussionPanel frontend

--- src/components/DiscussionPanel/DiscussionPanel.js
+++ src/components/DiscussionPanel/DiscussionPanel.js
@@ -1,14 +1,15 @@
 /* referred to VoteChain/src/components/Login/Login.js for page setup */
 
 import React from "react";
+import { io } from 'socket.io-client';
 import styles from "./DiscussionPanel.module.css";
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
 
 function DiscussionPanel() {
-    //FIXME: add any handling here if needed
-    const [values, setValues] = useState({  //FIXME: refer to Login.js for this
-        message: ""
-      });
+    //connect to server
+
+    //const socket = io("http://localhost:3000"); // Ensure this matches the backend's URL
+
 
     return (
         //FIXME: figure out how to get submission history removed from Form -> it currently shows past messages D:<
@@ -17,21 +18,14 @@ function DiscussionPanel() {
             <div className={styles.innerBox}>
                 <h1 className={styles.heading}>Discussion</h1>
 
-                <Form>
-                <FormGroup>
-                    <Label for="message">Post to Discussion Panel</Label>
-                    <Input
-                        type="message"
-                        id="message"
-                        placeholder="Type message... Please be mindful when participating!"
-                        value={values.message}
-                        // onChange={(event) =>
-                        //   setValues((prev) => ({ ...prev, email: event.target.value }))
-                        // }
-                    />
-                </FormGroup>
-
+                <Form className={styles.formContainer} id="msgForm" action="">
+                    <Input 
+                        type="text"
+                        name="msg"
+                        placeholder="Post to Discussion Panel"/>
+                    <Button className={styles.sendButton}>Send</Button>
                 </Form>
+                <div id="messages"></div>
             </div>
         </Container>
     );
