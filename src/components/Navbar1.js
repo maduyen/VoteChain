65a32f777e9b5c6f7213d453ad6c8099f1964dd6
ZLXW <zxweng@ucdavis.edu>
2024-12-08 04:07:18 -0800

    Navbar update
    After user login, the "user login" button exhibit "logined" and the "login out" button appear

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
   const linkStyle = { color: "white", marginRight: "15px" };
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if publicKey exists in sessionStorage
+    const publicKey = sessionStorage.getItem("publicKey");
+    setIsLoggedIn(!!publicKey); // Update state based on presence of publicKey
+  }, []);
+
+  const handleClick = (e) => {
+    if (isLoggedIn) {
+      alert("You are already logged in!");
+    }
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("publicKey"); // Clear publicKey from sessionStorage
+    setIsLoggedIn(false); // Update state
+    alert("You are logged out!");
+  };
 
   return (
     <AppBar position="fixed" style={{ backgroundColor: "#a2856d" }}>
@@ -15,15 +35,23 @@ const Navbar = () => {
         </Typography>
 
         <div>
-          <Button component={Link} to="/login" style={linkStyle}>
-            Voter Login
+          <Button component={Link} to="/polls" style={linkStyle}>
+            VIEW POLLS
           </Button>
           <Button component={Link} to="/create-poll" style={linkStyle}>
             CREATE POLL
-          </Button>
+          </Button>                    
           <Button component={Link} to="/userinfo" style={linkStyle}>
             YOUR VOTES
           </Button>
+          <Button component={Link}  to={isLoggedIn ? "#" : "/login"}  onClick={handleClick} style={linkStyle} >
+            {isLoggedIn ? "Logined" : "VOTER LOGIN"}
+          </Button>
+          {isLoggedIn && (
+            <Button style={linkStyle} onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
         </div>
       </Toolbar>
     </AppBar>
