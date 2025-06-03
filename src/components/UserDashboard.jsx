7deb5610f268ee91cf094b4dc75b309ada4bf697
Madelyn Nguyen <madelynguyen@gmail.com>
2025-06-02 23:50:52 -0700

    finished userDashboard UI

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -3,8 +3,14 @@ import Navbar from "./Navbar1";
 import Footer from "./Footer2";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import dashboardAvatar from "./images/dashboardAvatar.svg";
+import dashboardHistory from "./images/dashboardHistory.svg"
+import dashboardCreatePoll from "./images/dashboardCreatePoll.svg";
 
 const UserDashboard = () => {
+    const navigate = useNavigate(); // Hook for navigation
+
     const publicKey = sessionStorage.getItem('publicKey');
     const linkStyle = { color: "white", marginRight: "15px" };
 
@@ -45,21 +51,83 @@ const UserDashboard = () => {
             <div className="min-h-screen flex flex-col px-6 mt-[100px]">
             <h1 className="font-medium text-[#f5cfa8] text-4xl mb-4">Dashboard</h1> 
                 {/* Body */}
-                <div className="min-h-screen flex flex-row">
+                <div className="flex flex-row items-start">
                     {/* Side Bar */}
-                    <div className="w-[300px] max-h bg-[#312c51] rounded-[50px] shadow-lg flex-col mr-4">
-
-                        <h2 className="text-white text-2xl"> Welcome Back, </h2>   
-                        <h2 className="text-white text-2xl"> {userName || "Voter"} </h2>   
-                        <p className="text-white mb-4">Public Key: {publicKey}</p> {/* Display Public Key */}
-                        <Button component={Link} to="/updateprofile" style={linkStyle}>
+                    <div className="w-[350px] bg-[#f0c38e] rounded-[50px] shadow-lg flex flex-col p-4 mr-5">
+                        <img
+                            src={dashboardAvatar}
+                            alt="..."
+                            className="img-fluid floating mx-auto"
+                            style={{ width: "180px", height: "180px", marginTop: "5px", marginBottom: "20px"}}
+                        />
+                        {/* Welcome Message for User */}
+                        <h2 className="font-bold text-[#312c51] text-2xl"> Welcome Back, </h2>   
+                        <h2 className="font-medium text-[#312c51] text-2xl text-center pb-3"> {userName || "Voter"} </h2> 
+                        {/* About Section (optional) */}
+                        <p className="text-[#312c51] text-sm px-2 mb-4 text-center italic">
+                            {about }
+                        </p>
+                        {/* Update Profile Button */}
+                        <Button
+                            style={{
+                                backgroundColor: "#312c51",
+                                color: "white",
+                                marginBottom: "20px"
+                            }}
+                            onClick={() => navigate("/updateprofile")} // Navigate to UpdateProfile
+                            >
                             Edit Profile
                         </Button>
                     </div>
-                    <div className="max-w h-[200px] bg-[#312c51] rounded-[50px] shadow-lg flex-col">
-                         <Button component={Link} to="/userinfo" style={linkStyle}>
-                            YOUR VOTES
-                        </Button>
+                    {/* Dashboard Options */}
+                    <div className="min-h-screen flex flex-col">
+                        {/* Top Row */}
+                        {/* PublicKey container */}
+                        <div className="max-w h-[110px] bg-[#312c51] rounded-[25px] shadow-lg flex-col p-4 mb-4">
+                            <p className="font-bold text-xl text-white mb-1">Public Key:</p> {/* Display Public Key */}
+                            <p className="text-white break-words">{publicKey}</p>
+                        </div>
+                        {/* Bottom Row */}
+                        <div className="min-h-screen flex flex-row">
+                            {/* Vote History container */}
+                            <div className="w-[450px] h-[325px] bg-[#312c51] rounded-[50px] shadow-lg flex flex-col items-center justify-center mr-5">
+                                    <img
+                                    src={dashboardHistory}
+                                    alt="..."
+                                    className="img-fluid floating mx-auto"
+                                    style={{ width: "180px", height: "180px", marginBottom: "20px"}}
+                                />
+                                <Button
+                                    style={{
+                                        marginTop: "10px",
+                                        backgroundColor: "#f0c38e",
+                                        color: "#312c51"
+                                    }}
+                                    onClick={() => navigate("/userinfo")} // Navigate to PollsListPage
+                                    >
+                                    View Vote History
+                                </Button>
+                            </div>
+                            {/* Create Poll container */}
+                            <div className="w-[350px] h-[325px] bg-[#312c51] rounded-[50px] shadow-lg flex flex-col items-center justify-center">
+                                    <img
+                                    src={dashboardCreatePoll}
+                                    alt="..."
+                                    className="img-fluid floating mx-auto"
+                                    style={{ width: "180px", height: "180px", marginBottom: "20px"}}
+                                />
+                                <Button
+                                    style={{
+                                        marginTop: "10px",
+                                        backgroundColor: "#f0c38e",
+                                        color: "#312c51"
+                                    }}
+                                    onClick={() => navigate("/create-poll")} // Navigate to PollsListPage
+                                    >
+                                    Create Poll
+                                </Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
         
